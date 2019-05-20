e1d63524b570fb48b9b4c347cd0871a0825a709f
Sami <bmb,sami@yahoo.com>
2019-05-20 20:33:00 +0300

    product service integration test

--- src/test/java/org/fasttrackit/onlineshopapi/ProductServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/onlineshopapi/ProductServiceIntegrationTests.java
@@ -1,5 +1,6 @@
-package onlineshopapi;
+package org.fasttrackit.onlineshopapi;
 
+import org.fasttrackit.onlineshopapi.domain.Product;
 import org.fasttrackit.onlineshopapi.service.ProductService;
 import org.fasttrackit.onlineshopapi.transfer.CreateProductRequest;
 import org.junit.Test;
@@ -8,9 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.IsNull.notNullValue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProductServiceIntegrationTests {
+
 
 	@Autowired
 	private ProductService productService;
@@ -23,8 +30,15 @@ public class ProductServiceIntegrationTests {
 		request.setPrice(99.9);
 		request.setQuantity(10);
 
-		productService.createProduct(request);
+		Product createdProduct = productService.createProduct(request);
 
+
+		//import static methods
+		assertThat(createdProduct,notNullValue());
+		assertThat(createdProduct.getId(),greaterThan(new Long(0)));
+		assertThat(createdProduct.getName(),is(request.getName()));
+		assertThat(createdProduct.getPrice(),is(request.getPrice()));
+		assertThat(createdProduct.getQuantity(),is(request.getQuantity()));
 	}
 
 }
